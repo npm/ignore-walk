bd6f030be7d5a31181d66bfb3caef0f1fa0bb20a
Christoph Thiede <christoph.thiede@outlook.de>
2021-10-23 22:44:45 +0000

    Fix recursive directory ignore rules
    Citing from the gitignore docs [1]:
    
    > If there is a separator at the beginning or middle (or both) of the
    > pattern, then the pattern is relative to the directory level of the
    > particular .gitignore file itself. Otherwise the pattern may also
    > match at any level below the .gitignore level.
    
    Previously, parent walkers were only passed the resolved path to a file
    entry via filterEntry(). This patch also passes the entry's basename
    which is matched against the rule if it is relative.
    
    Regression tests are included as well.
    
    Closes #9.
    
    [1] http://git-scm.com/docs/gitignore#_pattern_format#:~:text=If%20there%20is%20a%20separator%20at%20the%20beginning%20or%20middle%20(or%20both)%20of%20the%20pattern%2C%20then%20the%20pattern%20is%20relative%20to%20the%20directory%20level%20of%20the%20particular%20.gitignore%20file%20itself.%20Otherwise%20the%20pattern%20may%20also%20match%20at%20any%20level%20below%20the%20.gitignore%20level.

--- test/nested-ignores.js
+++ test/nested-ignores.js
@@ -7,16 +7,24 @@ const path = resolve(__dirname, 'fixtures')
 // set the ignores just for this test
 var c = require('./common.js')
 c.ignores({
-  '.ignore': ['*', 'd', 'h', '!d/c/h/.dch', '!/h/c/d/hcd'],
+  '.ignore': ['*', 'd', 'h', '!d/c/h/.dch', '!/h/c/d/hcd', '!c/**/ccc'],
   'd/.ignore': ['!*', '.ignore'], // unignore everything
   'd/d/.ignore': ['*'], // re-ignore everything
   'd/c/.ignore': ['*', '!/h/.dch'], // original unignore
   'd/h/.ignore': ['*'], // ignore everything again
   'h/c/d/.ignore': ['!hcd', '!.hcd', '!/d{ch,dd}'],
+  'c/.ignore': ['h/', 'c/c*'], // ignore directories recursively (GH#9)
 })
+// For GH#9, we need one more level of depth
+c.createFile('c/c/d/d', 'ccc')
+c.createFile('c/c/d/h', 'ccc')
 
 // the only files we expect to see
 var expected = [
+  'c/c/d/ccc',
+  'c/c/d/d/ccc',
+  'c/d/c/ccc',
+  'c/d/d/ccc',
   'd/c/h/.dch',
   'h/c/d/.hcd',
   'h/c/d/dch',
