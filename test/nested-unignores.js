b587cc32f430dc8d3052cb2905416e301f26df0d
Mohamed Akram <mohd-akram@users.noreply.github.com>
2023-11-29 19:43:19 +0400

    fix: handling of nested unignores (#119)
    The fix in #116 was not quite complete. git does allow unignoring
    directories as long as its path to the root is unignored. This modifies
    the check to handle this case. The test case in `nested-ignores.js` was
    indeed wrong because it interpreted unignoring a specific file
    `!/h/c/d/hcd` as unignoring the directory `!/h/c/d`. I added new tests
    that specifically unignore a directory and therefore allow unignoring
    anything else within it.
    
    ## References
    Related to #116

--- test/nested-unignores.js
+++ test/nested-unignores.js
@@ -7,12 +7,25 @@ const path = resolve(__dirname, 'fixtures')
 // set the ignores just for this test
 var c = require('./common.js')
 c.ignores({
-  '.ignore': ['*', '!/c', '!/d'],
+  '.ignore': ['*', '!/c/d', '!/d/c', '!/d/d/', '!/d/h/h'],
   'c/.ignore': ['!*', '.ignore'], // unignore everything
+  'd/c/.ignore': ['!h', '!cc*'], // unignore directory
+  'd/d/.ignore': ['!h/', '!cc*'], // unignore directory with slash
+  'd/h/h/.ignore': ['!dd*'], // unignore files
 })
 
 // the only files we expect to see
-var expected = []
+var expected = [
+  'd/c/h/ccc',
+  'd/c/h/ccd',
+  'd/c/h/cch',
+  'd/d/h/ccc',
+  'd/d/h/ccd',
+  'd/d/h/cch',
+  'd/h/h/ddc',
+  'd/h/h/ddd',
+  'd/h/h/ddh',
+]
 
 const t = require('tap')
 
